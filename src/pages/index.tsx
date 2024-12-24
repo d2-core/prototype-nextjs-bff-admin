316d9dc9ec15704dd00c5184f85a3bf132d2ca31
d2-core <dohaktae88@gmail.com>
2024-12-24 20:44:24 +0900

    homepage template 반영

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
-import MainBox from '@/components/shared/MainBox'
+import useUser from '@/hooks/auth/useUser'
+import { Box, Button } from '@mui/material'
+import { useRouter } from 'next/router'
+import { useCallback } from 'react'
 
 export default function Home() {
-  return <MainBox children={<div>Hello</div>} />
+  const route = useRouter()
+  const { user } = useUser()
+  const handleLoginPageRoute = useCallback(() => {
+    route.push('/auth/login')
+  }, [route])
+
+  const handleWorkspaceRoute = useCallback(() => {
+    route.push('/workspace')
+  }, [route])
+
+  const randerAuth = useCallback(() => {
+    if (user) {
+      return <Button onClick={handleWorkspaceRoute}>Workspace</Button>
+    } else {
+      return <Button onClick={handleLoginPageRoute}>로그인</Button>
+    }
+  }, [route])
+  return (
+    <Box>
+      <div>Creation Home</div>
+      {randerAuth()}
+    </Box>
+  )
 }
