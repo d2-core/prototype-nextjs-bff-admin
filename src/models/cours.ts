32751c5a8e14f0167fc286ae7b7615fb5a0778af
d2-core <dohaktae88@gmail.com>
2024-12-26 20:06:01 +0900

    cours model 설계

--- src/models/cours.ts
+++ src/models/cours.ts
@@ -1,4 +1,34 @@
-interface ICours {
+import { CoursCategory, CoursLebvel } from './form'
+
+export interface Cours {
   id: number
-  title: string
+  thumbnailImageUrls: string[]
+  category?: CoursCategory
+  title?: string
+  description?: string
+  level?: CoursLebvel
+  tags: string[]
+  price?: number
+  duration?: number
+  author?: string
+  rating?: number
+  enrolledCount?: number
+  createdAt?: string
+  updatedAt?: string
+  isPublish?: boolean
+  publishedAt?: string
+  courseStatistics?: CourseDetailStatistics
+}
+
+export interface CourseDetailStatistics {
+  studentStatistics: {
+    totalStudents: number
+    activeStudents: number
+    completedStudents: number
+  }
+  lectureStatistics: {
+    totalLectures: number
+    totalDuration: number
+    averageDuration: number
+  }
 }
