56f99cd20403897eb4a8c06bf5274a515f3fc950
d2-core <dohaktae88@gmail.com>
2025-01-03 07:15:42 +0900

    course api 추가

--- src/remote/api/course.ts
+++ src/remote/api/course.ts
@@ -1,11 +1,14 @@
-import { Cours } from '@/models/cours'
+import { Cours, CoursePublishState, CourseTeacher } from '@/models/cours'
 import { getApi } from './axios'
 import { Api } from '@/models/api'
 import { CoursForm } from '@/models/form'
+import { objectStorageUrl } from '@/constants/api'
+import { AxiosResponse } from 'axios'
 
 export async function createCourse(courseForm: CoursForm): Promise<Api<Cours>> {
   const formData = createCourseFormData(courseForm)
-  const { data } = await getApi('PRODUCT').post(
+
+  const { data }: AxiosResponse<Api<Cours>> = await getApi('PRODUCT').post(
     'api/product/v1/courses',
     formData,
     {
@@ -15,33 +18,135 @@ export async function createCourse(courseForm: CoursForm): Promise<Api<Cours>> {
     },
   )
 
-  return data
+  return {
+    ...data,
+    body: {
+      ...data.body,
+      thumbnailImageUrls: data.body.thumbnailImageUrls?.map(
+        (url: string) => `${objectStorageUrl}/${url}`,
+      ),
+    },
+  }
+}
+
+export async function updateCourse({
+  courseId,
+  courseForm,
+}: {
+  courseId: number
+  courseForm: CoursForm
+}): Promise<Api<Cours>> {
+  const formData = createCourseFormData(courseForm)
+  const { data }: AxiosResponse<Api<Cours>> = await getApi('PRODUCT').put(
+    `api/product/v1/courses/${courseId}`,
+    formData,
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    },
+  )
+
+  return {
+    ...data,
+    body: {
+      ...data.body,
+      thumbnailImageUrls: data.body.thumbnailImageUrls?.map(
+        (url: string) => `${objectStorageUrl}/${url}`,
+      ),
+    },
+  }
 }
 
 export async function getCourseList(): Promise<Api<Cours[]>> {
-  const { data } = await getApi('PRODUCT').get('api/product/v1/courses')
+  const { data }: AxiosResponse<Api<Cours[]>> = await getApi('PRODUCT').get(
+    'api/product/v1/courses',
+  )
+
+  return {
+    ...data,
+    body: data.body.map((course) => ({
+      ...course,
+      thumbnailImageUrls: course.thumbnailImageUrls?.map(
+        (url: string) => `${objectStorageUrl}/${url}`,
+      ),
+    })),
+  }
+}
+
+export async function getCourseTeacher({
+  courseId,
+}: {
+  courseId: number
+}): Promise<Api<CourseTeacher>> {
+  const { data }: AxiosResponse<Api<CourseTeacher>> = await getApi(
+    'PRODUCT',
+  ).get(`api/product/v1/courses/${courseId}/teachers`)
+
+  return data
+}
+
+export async function getCourse({
+  courseId,
+}: {
+  courseId: number
+}): Promise<Api<Cours>> {
+  const { data }: AxiosResponse<Api<Cours>> = await getApi('PRODUCT').get(
+    `api/product/v1/courses/${courseId}`,
+  )
+
+  return {
+    ...data,
+    body: {
+      ...data.body,
+      thumbnailImageUrls: data.body.thumbnailImageUrls?.map(
+        (url: string) => `${objectStorageUrl}/${url}`,
+      ),
+    },
+  }
+}
+
+export async function deleteCourse({
+  courseId,
+}: {
+  courseId: number
+}): Promise<Api<any>> {
+  const { data }: AxiosResponse<Api<any>> = await getApi('PRODUCT').delete(
+    `api/product/v1/courses/${courseId}`,
+  )
+
+  return data
+}
+
+export async function publishCourse(body: {
+  courseId: number
+  publishState: CoursePublishState
+}): Promise<Api<any>> {
+  const { data }: AxiosResponse<Api<any>> = await getApi('PRODUCT').post(
+    `api/product/v1/courses/publish`,
+    body,
+  )
 
   return data
 }
 
 function createCourseFormData(courseForm: CoursForm): FormData {
   const formData = new FormData()
-
-  Object.entries(courseForm).forEach(([key, value]) => {
-    if (key === 'thumbnailImageFiles' && value.length === 0) {
-      formData.append(key, JSON.stringify([]))
-    } else if (Array.isArray(value)) {
-      value.forEach((item, index) => {
-        if (item instanceof File) {
-          formData.append(`${key}[${index}]`, item)
-        } else {
-          formData.append(`${key}[${index}]`, item)
-        }
-      })
-    } else {
-      formData.append(key, value as string)
-    }
+  courseForm.thumbnailImageFiles.forEach((fileForm, index) => {
+    fileForm.file != null &&
+      formData.append(`thumbnailImageFiles[${index}].file`, fileForm.file)
+    formData.append(`thumbnailImageFiles[${index}].url`, fileForm.url)
   })
+
+  formData.append('courseCategoryId', String(courseForm.courseCategoryId))
+  formData.append('title', courseForm.title)
+  formData.append('subTitle', courseForm.subTitle)
+  formData.append('descriptionWithMarkdown', courseForm.descriptionWithMarkdown)
+  formData.append('courseLevelId', String(courseForm.courseLevelId))
+  courseForm.tags.forEach((tag, index) => {
+    formData.append(`tags[${index}]`, tag)
+  })
+  formData.append('price', String(courseForm.price))
 
   return formData
 }
