9f8ed8a0f63403ebdfd25286a5161d882f053862
d2-core <dohaktae88@gmail.com>
2025-01-03 07:16:04 +0900

    course edit, detail route parse logic 추가

--- src/pages/workspace/cours/[id].tsx
+++ src/pages/workspace/cours/[id].tsx
@@ -1,8 +1,12 @@
 import CoursDetail from '@/components/cours/CoursDetail'
 import MainBox from '@/components/shared/MainBox'
+import { useRouter } from 'next/router'
 
 function CoursDetailPage() {
-  return <MainBox children={<CoursDetail />} />
+  const route = useRouter()
+  const { id } = route.query
+
+  return <MainBox children={<CoursDetail id={parseInt(id as string)} />} />
 }
 
 export default CoursDetailPage
--- src/pages/workspace/cours/edit/[id].tsx
+++ src/pages/workspace/cours/edit/[id].tsx
@@ -1,18 +1,17 @@
 import CoursForm from '@/components/cours/CoursForm'
 import MainBox from '@/components/shared/MainBox'
-import qs from 'qs'
-import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
 function CoursModify() {
-  const [id, setId] = useState<number>()
+  const router = useRouter()
+  const { id } = router.query
 
-  useEffect(() => {
-    const { id } = qs.parse(location.search, {
-      ignoreQueryPrefix: true,
-    }) as { id: string }
-    setId(parseInt(id))
-  }, [])
-
-  return <MainBox children={<CoursForm id={Number(id)} />} />
+  console.log(id)
+  return (
+    <MainBox>
+      <CoursForm id={parseInt(id as string)} />
+    </MainBox>
+  )
 }
+
 export default CoursModify
